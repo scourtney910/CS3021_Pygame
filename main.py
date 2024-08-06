3d425db78aa080e60aeafeca094f1f39d7d09fe2
Sean Courtney <stcbklyn@gmail.com>
2024-08-06 12:41:23 -0700

    Update main file with more documentation

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import pygame
-import sys
 import random
+import sys
+from typing import Any, Union
 import math
-import os
-
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 
 # Constants
@@ -12,11 +10,10 @@ SCREEN_WIDTH      = 800
 SCREEN_HEIGHT     = 600
 FPS               = 60
 GRAVITY           = 0.5
-PLAYER_WIDTH      = 50 #use to change player size
+PLAYER_WIDTH      = 50
 PLAYER_HEIGHT     = 50
 PLATFORM_WIDTH    = 100
 PLATFORM_HEIGHT   = 20
-# PLATFORM_COLOR    = (0, 255, 0)
 PLATFORM_COLOR    = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 MAX_JUMP_STRENGTH = 20
 
@@ -26,7 +23,7 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Jump King + Doodle Jump')
 clock = pygame.time.Clock()
-
+font = pygame.font.SysFont(None, 55)
 
 
 class Player(pygame.sprite.Sprite):
@@ -34,8 +31,9 @@ class Player(pygame.sprite.Sprite):
 
     def __init__(self):
         """Initialize all the player attributes"""
+        #Player uses has-a relationship from pygame
+        super().__init__() #take attributes form pygame module, inheriting attributes from pygame
 
-        super().__init__() #take attributes form pygame module, inheriting attributes from pygame has is a ?
         self.original_image = pygame.image.load('player.png').convert_alpha()
         self.image = pygame.transform.scale(
             self.original_image,
@@ -43,26 +41,21 @@ class Player(pygame.sprite.Sprite):
             )
 
         self.rect = self.image.get_rect() #forcing player hot box into  a square
-        self.rect.center = ( #starting location for sprite
-            SCREEN_WIDTH // 2, 
-            SCREEN_HEIGHT - self.rect.height // 2
-            )
 
         self.velocity_y = 0
         self.velocity_x = 0
         self.is_jumping = False
 
 
-    def jump(self, vector):
-
+    def jump(self, vector: tuple) -> None:
+        """change player state to recognize sprite jumping
+        vector expected is from the calculate_vector function"""
         self.velocity_x, self.velocity_y = vector
         self.is_jumping = True
 
 
     def update(self):
-        """
-        update player position on screen based on changing attributes
-        """
+        """update player position on screen based on changing attributes"""
 
         if self.is_jumping:
 
@@ -72,9 +65,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.x += self.velocity_x
 
             if self.rect.bottom >= SCREEN_HEIGHT:
-
-                self.rect.bottom = SCREEN_HEIGHT
-                self.is_jumping = False
+                # Game Over is set
+                return True
 
         # Wrap around the screen
         if self.rect.right < 0:
@@ -82,31 +74,36 @@ class Player(pygame.sprite.Sprite):
 
         elif self.rect.left > SCREEN_WIDTH:
             self.rect.right = 0
+        
+        # All previous checks did not change the state of play
+        return False
 
 
-    def check_collision(self, platforms):
+    def check_collision(self, platforms: Any) -> None:
         """
         Check for collision only when falling.
         To change the platforms structure,
         change the generate_platforms function below.
-        Need to add collisin for bottom of platforms
+        ***Need to add collision for bottom of platforms***
         """
-        if self.velocity_y > 0: #sprite is falling, pos vel in y means falling and neg means jumping
+
+        #"if sprite is falling", (+)velocity_y means falling and neg means jumping
+        if self.velocity_y > 0:
 
             hits = pygame.sprite.spritecollide(self, platforms, False) #hits is a list of something about collisions
-            """ sprite collide checks for collision of sprite and platforms and returns lists"""
-
-            if hits: #if list is not empty
+            # sprite collide checks for collision of sprite and platforms and returns lists
+            
+            #if hits != None, i.e., if the player is hitting something
+            if hits:
                 # this code for bottom of char and top of platform
-                #shoudl use if statement for bottom and another if statement for top
-                self.rect.bottom = hits[0].rect.top 
-                """assigning player bottom to top of platform represent by rect top, i.e. only wokrs because the only thign player can hit is a platform"""
+                # should use if-statement for bottom and another if-statement for top
+                
+                self.rect.bottom = hits[0].rect.top
+                # assigning player bottom to top of platform represented by rect top,
+                # i.e. only wokrs because the only thing the player can hit is a platform
+                
                 self.velocity_y = 0
                 self.is_jumping = False
-                #if top of sprite hits bottom of platform:
-                #self.velocity = 0
-                #self.is_jumping = False
-                
 
 
 class Platform(pygame.sprite.Sprite):
@@ -123,19 +120,26 @@ class Platform(pygame.sprite.Sprite):
         self.rect.y = y
 
 
-def generate_platforms(num_platforms: int) -> set: 
-    #returning a tuple  each tuple represents a platform, inside the tuple are the nuts and bolts of each platform
+def generate_platforms(num_platforms: int) -> tuple[Any, Any]:
+    """randomly generate rectangle platforms and return:
+    pygame.sprite.Group: Any, lowest_platform.sprite: Any"""
 
     platforms = pygame.sprite.Group()
+    lowest_y = 0
+    lowest_platform = None
 
     for _ in range(num_platforms):
 
         x = random.randint(0, SCREEN_WIDTH - PLATFORM_WIDTH)
         y = random.randint(0, SCREEN_HEIGHT - PLATFORM_HEIGHT)
-        platform = Platform(x, y) # object / isntantiation of platform class
-        platforms.add(platform) #adding the instanced platform to group Platform
+        platform = Platform(x, y)   # object / isntantiation of platform class
+        platforms.add(platform)     # adding the instanced platform to group Platform
 
-    return platforms
+        if y > lowest_y:
+            lowest_y = y            #update the if-condition for every for-loop iteration
+            lowest_platform = platform    #update the future return value
+
+    return platforms, lowest_platform
 
 
 def calculate_vector(start_pos: tuple, end_pos: tuple) -> tuple:
@@ -160,54 +164,84 @@ def calculate_vector(start_pos: tuple, end_pos: tuple) -> tuple:
 
     return (vector_x, vector_y)
 
+def display_text(text, color, x, y) -> None:
+    """Helper function to set rendering for Game Over Screen"""
+    img = font.render(text, True, color)
+    screen.blit(img, (x, y))
+
 
 ##############################################################
 ############################ MAIN ############################
 ##############################################################
+
+
 def main() -> None:
     """Run the main game loop here. Program exits when game is killed."""
 
-    # Load background image
+    # Load background image and sprites
     background_image = pygame.image.load('background.png').convert() # loading image
-    player = Player() #create sprite
-    all_sprites = pygame.sprite.Group() #might need to make a function to create characters for multiplayer
+    player = Player() # create sprite
+    platforms, lowest_platform = generate_platforms(10) # manually choose number of platforms here
+    player.rect.center = (lowest_platform.rect.centerx, lowest_platform.rect.top - PLAYER_HEIGHT // 2)
+    
+    # these lines work when creating one player, but will be moved when creating multiplayer
+    all_sprites = pygame.sprite.Group() 
     all_sprites.add(player)
-
-    platforms = generate_platforms(10) #manually chose number of platforms here
     all_sprites.add(platforms)
 
+    #initialize starting values
     dragging = False
-    start_pos = (0, 0) # initializing tuple here for mouse position, change name
+    start_pos = (0, 0) # initialize tuple here for mouse position
+    score = 0
+    game_over = False
 
+    #main game-loop
     while True:
-
-        for event in pygame.event.get(): #event is class in pygame to check for certain conditions, like quit
-            if event.type == pygame.QUIT:
+        
+        # events are a method in pygame to check for certain conditions
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # red x on top right of window
                 pygame.quit()
                 sys.exit()
 
-            elif event.type == pygame.MOUSEBUTTONDOWN: #left click
+            elif event.type == pygame.MOUSEBUTTONDOWN: # left click
 
-                if not player.is_jumping: #if player isnt jumping allow click
+                # if player isn't jumping, allow click
+                if not player.is_jumping:
                     dragging = True
                     start_pos = pygame.mouse.get_pos()
 
-            elif event.type == pygame.MOUSEBUTTONUP: #release left click
+            elif event.type == pygame.MOUSEBUTTONUP: # release left click
 
                 if dragging:
                     end_pos = pygame.mouse.get_pos()
                     vector = calculate_vector(start_pos, end_pos)
                     player.jump(vector)
                     dragging = False
-        
-        player.update() #player movement function
-        player.check_collision(platforms) #only checking for collisions after exiting update, whcih means my y velocity is pos/downwards
 
-        screen.blit(background_image, (0, 0)) #centering background image
-        all_sprites.draw(screen) #drawing updated character postion onto screen including platforms
+        #Increment the score only when the game is not over
+        #And the player made a successful jump
+        if not game_over:
+            game_over = player.update() # player movement takes place here
+            player.check_collision(platforms) # check for collisions after sprite is updated each frame
+            if not player.is_jumping:
+                score += 1
+
+        # background image setter
+        # randomize background_image later
+        screen.blit(background_image, (0, 0))
+
+        # drawing updated character position each frame.
+        # Would apply to platforms if they were also moving
+        all_sprites.draw(screen) 
         
+        if game_over:
+            # display_text(str, color, x_pos, y_pos)
+            display_text('Game Over', (255, 0, 0), (SCREEN_WIDTH // 2 - 100), (SCREEN_HEIGHT // 2 - 50))
+            display_text(f'Score: {score}', (255, 255, 255), (SCREEN_WIDTH // 2 - 100), (SCREEN_HEIGHT // 2))
+
         pygame.display.flip()
-        clock.tick(FPS) #ticking forward clock
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
